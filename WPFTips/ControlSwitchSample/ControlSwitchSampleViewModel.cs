72bb0454da3b30be595181aee1ca5877b44f0463
Akn <sibata486@gmail.com>
2020-08-14 00:09:38 +0900

    機能追加 / ビューに ContentControl を設置し、その中に ListView を格納

--- WPFTips/ControlSwitchSample/ControlSwitchSampleViewModel.cs
+++ WPFTips/ControlSwitchSample/ControlSwitchSampleViewModel.cs
@@ -6,5 +6,15 @@ using System.Threading.Tasks;
 
 namespace WPFTips.ControlSwitchSample {
     class ControlSwitchSampleViewModel {
+
+        private List<String> strings = new List<string>(new String[] { "el1", "el2", "el3" });
+
+        public List<String> Strings {
+            get => strings;
+            set {
+                strings = value;
+            }
+        }
+
     }
 }
